5fbd0410180b0877e25ed1e383cda2de708a8b50
campolake <campolake@gmail.com>
2013-10-29 00:20:06 +0800

    init

---
+++ weixin/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using weixin.Filters;
+using weixin.Models;
+
+namespace weixin.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        public ActionResult getpwd()
+        {
+            return View();
+        }
+    }
+}
